d80fef0d4065e6d644e4598338d11854f5a9d247
Durgesh Singh <57593854+DurgeshSingh1@users.noreply.github.com>
2024-06-22 00:47:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 app.use(
-  cors({ origin: "https://blog-durgesh.netlify.app/", credentials: true })
+  cors()
 ); // { origin: "http://localhost:5173", credentials: true }
 app.use(cookieParser());
 app.use("/", authRoute);
